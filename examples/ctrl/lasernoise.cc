834d2ca29638a800a8590c5d803a9e20e7486458
Adrian Böckenkamp <adrian.boeckenkamp@tu-dortmund.de>
2017-05-30 23:49:45 +0200

    lasernoise: make noise in ctrl plugin configurable (mean and standard deviation)

--- examples/ctrl/lasernoise.cc
+++ examples/ctrl/lasernoise.cc
@@ -1,21 +1,28 @@
 /*
-   File rangernoise.cc: ranger noise plugin demo for Stage
-   Author: Richard Vaughan
-   Date: 3 March 2008
-   CVS: $Id: rangernoise.cc,v 1.1 2008-03-04 02:09:56 rtv Exp $
+   File rangernoise.cc: (configurable) ranger noise plugin demo for Stage
+   Author: Richard Vaughan, Adrian Böckenkamp
+   Date: 30 May 2017
 */
 
 #include "stage.hh"
+
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
 using namespace Stg;
 
-const double DEVIATION = 0.05;
+double mean = 1.0;
+double stddev = 0.05;
 
-double simple_normal_deviate(double mean, double stddev)
+double SimpleNormalDeviate()
 {
   double x = 0.0;
 
-  for (int i = 0; i < 12; i++)
+  for (unsigned short i = 0; i < 12; i++) {
     x += rand() / (RAND_MAX + 1.0);
+  }
 
   return (stddev * (x - 6.0) + mean);
 }
@@ -24,20 +31,50 @@ double simple_normal_deviate(double mean, double stddev)
 int RangerUpdate(ModelRanger *mod, void *)
 {
   // get the data
-  std::vector<meters_t> &scan = mod->GetSensorsMutable()[0].ranges;
+  vector<meters_t> &scan = mod->GetSensorsMutable()[0].ranges;
 
-  if (scan.size() > 0)
-    FOR_EACH (it, scan)
-      *it *= simple_normal_deviate(1.0, DEVIATION);
+  if (scan.size() > 0) {
+    FOR_EACH (it, scan) {
+      *it *= SimpleNormalDeviate();
+    }
+  }
 
   return 0; // run again
 }
 
+void SplitArgs(const string &text, const string &separators, vector<string> &words)
+{
+  int n = text.length();
+  int start = text.find_first_not_of(separators);
+  while ((start >= 0) && (start < n)) {
+    int stop = text.find_first_of(separators, start);
+    if ((stop < 0) || (stop > n)) {
+      stop = n;
+    }
+    words.push_back(text.substr(start, stop - start));
+    start = text.find_first_not_of(separators, stop + 1);
+  }
+}
+
 // Stage calls this when the model starts up. we just add a callback to
 // the model that gets called just after the sensor update is done.
-extern "C" int Init(Model *mod)
+extern "C" int Init(Model *mod, CtrlArgs *args)
 {
-  mod->AddCallback(Model::CB_UPDATE, (model_callback_t)RangerUpdate, NULL);
+  srand(time(0));
+
+  // tokenize the argument string into words
+  vector<string> words;
+  SplitArgs(args->worldfile, string(" \t"), words); // words[0] is the plugin name (= "lasernoise")
+  if (words.size() == 2) { // just one parameter? consider this to be standard deviation
+    stddev = atof(words[1].c_str());
+    //PRINT_MSG1("Setting stddev=%f of LaserNoiseV2 plugin.", stddev);
+  } else if (words.size() >= 3) { // two parameters given? this is mean and std. dev.
+    mean = atof(words[1].c_str());
+    stddev = atof(words[2].c_str());
+    //PRINT_MSG3("Setting mean=%f, stddev=%f of '%s' plugin.", mean, stddev, words[0].c_str());
+  }
+
+  mod->AddCallback(Model::CB_UPDATE, model_callback_t(RangerUpdate), NULL);
 
   // add this so we can see the effects immediately, without needing
   // anyone else to subscribe to the ranger
